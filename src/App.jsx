a5bebcd09030f93a3750f5d18721bd2fae3d684c
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-14 13:49:17 -0300

    Terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import Primeiro from "./components/Primeiro";
 import ComParametro from "./components/ComParametro";
 import ComFilhos from "./components/ComFilhos";
 import Card from "./components/layout/Card";
+import Condicional from "./components/Condicional";
 import './App.css'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (porps) => (
     <div className="App">
-        <Card titulo="#01 - Primeiro Componente"><Primeiro/></Card>
-        <Card titulo="#02 - Componente Com Paramentro">
-            <ComParametro titulo = "Esse é o título" 
-            subtitulo ="Esse é o subtitulo"/>
+
+        <Card titulo="#05 - Condicional">
+            <Condicional numero= {11}></Condicional>
         </Card>
         <Card titulo="#03 - Componente Com Filhos">
             <ComFilhos>
-            <ul>
-            <li>Ana</li>
-            <li>Bia</li>
-            <li>Carlos</li>
-            <li>Daniel</li>
-            </ul>
-             </ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
         </Card>
-          
-       </div>
+        <Card titulo="#01 - Primeiro Componente"><Primeiro /></Card>
+        <Card titulo="#02 - Componente Com Paramentro">
+            <ComParametro titulo="Esse é o título"
+                subtitulo="Esse é o subtitulo" />
+        </Card>
+    </div>
 )
---
+++ src/components/Condicional.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+
+export default props => {
+
+    return (
+        <div>
+            <h2>O número é {props.numero}</h2>
+            { props.numero % 2 == 0 ?
+            <span>Par</span>
+            :<span>Impar</span>
+            }
+        </div>
+    )
+}
